b78ee9034a8d057b9cdbc744dcb1ca7836c0d50f
engineerscodes <practicalengineers05@gmail.com>
2021-07-04 23:29:32 +0530

    task 4

---
+++ app/src/main/java/com/example/firstdemo/task_4.java
@@ -0,0 +1,13 @@
+package com.example.firstdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class task_4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_4);
+    }
+}
