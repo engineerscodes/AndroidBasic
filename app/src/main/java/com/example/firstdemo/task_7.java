382f05a45d3048fb312128ab01b1ae7c1f95abcb
engineerscodes <practicalengineers05@gmail.com>
2021-07-10 20:36:34 +0530

    task 7

--- app/src/main/java/com/example/firstdemo/task_7.java
+++ app/src/main/java/com/example/firstdemo/task_7.java
@@ -1,8 +1,13 @@
 package com.example.firstdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class task_7 extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class task_7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_7);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() ==R.id.namesList)
+            Toast.makeText(this," NAMES MENU ",Toast.LENGTH_SHORT).show();
+        if(item.getItemId() ==R.id.classids)
+            Toast.makeText(this," CLASS MENU",Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
